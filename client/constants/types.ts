184cc3b240fce5053ae09f38980948242815f787
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2025-05-22 00:50:11 +0600

    types: required for navigation

---
+++ client/constants/types.ts
@@ -0,0 +1,17 @@
+// types.ts
+export type RootStackParamList = {
+  Login: undefined;
+  Registration: undefined;
+  ContentScreen: undefined;
+  MainApp: undefined;
+};
+
+export type HomeStackParamList = {
+  Home: undefined;
+  Details: undefined;
+};
+
+export type TabParamList = {
+  HomeStack: undefined;
+  Daily: undefined;
+};
